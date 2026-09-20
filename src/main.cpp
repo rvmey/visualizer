061437089b1bfbd685e0c229a10731bd1992f866
Russell VanderMey <russell@vandermey.us>
2026-09-20 17:57:27 -0400

    Revert test change

--- src/main.cpp
+++ src/main.cpp
@@ -313,7 +313,7 @@ static void syncWindowSize(AppContext& ctx)
 
 static void updateWindowTitle(AppContext& ctx)
 {
-    std::string title = "MilkDrop Visualizer test 2";
+    std::string title = "MilkDrop Visualizer";
 
     uint32_t index = projectm_playlist_get_position(ctx.switcher->front.playlist);
     char* presetName = projectm_playlist_item(ctx.switcher->front.playlist, index);
